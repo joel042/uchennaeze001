0ee9436ba57ee5abf75cf1360a4b6ca6144388b8
joel042 <uchennajoeleze@gmail.com>
2024-06-09 11:34:10 +0100

    '.'

--- files/script.js
+++ files/script.js
@@ -1,10 +1,27 @@
 
-  // Dark Mode Toggle
-        const toggleButton = document.getElementById('dark-mode-toggle');
-        toggleButton.addEventListener('click', () => {
-            document.body.classList.toggle('dark-mode');
-            document.querySelector('.navbar').classList.toggle('dark-mode');
-            document.querySelector('.heroName').classList.toggle('dark-mode');
-            document.querySelector('.work-process').classList.toggle('dark-mode');
-        });
+  // // Dark Mode Toggle
+  //       const toggleButton = document.getElementById('dark-mode-toggle');
+  //       toggleButton.addEventListener('click', () => {
+  //           document.body.classList.toggle('dark-mode');
+  //           document.querySelector('.navbar').classList.toggle('dark-mode');
+  //           document.querySelector('.heroName').classList.toggle('dark-mode');
+  //           document.querySelector('.work-process').classList.toggle('dark-mode');
+  //       });
    
+
+  
+    document.addEventListener("DOMContentLoaded", function() {
+        const toggleButton = document.getElementById('dark-mode-toggle');
+        const body = document.body;
+
+        // Check for saved user preference, if any, on load of the website
+        if (localStorage.getItem('dark-mode') === 'true') {
+            body.classList.add('dark-mode');
+        }
+
+        toggleButton.addEventListener('click', function() {
+            body.classList.toggle('dark-mode');
+            // Save the user preference in local storage
+            localStorage.setItem('dark-mode', body.classList.contains('dark-mode'));
+        });
+    });
